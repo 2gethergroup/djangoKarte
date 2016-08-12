9dc02e980263c6910572b4147b679aa67cf4ba9a
Nemanja Paunić <paunicnemaja@gmail.com>
2016-08-12 03:47:30 +0200

    Bootstrap templejt nesređen

--- djangoKarte/djangoKarte/urls.py
+++ djangoKarte/djangoKarte/urls.py
@@ -13,10 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+#DODATO
+from django.conf import settings
 from django.conf.urls import include, url
+#DODATO
+from django.conf.urls.static import static
 from django.contrib import admin
 
+
+from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blogDjangoKarte.urls')),
-]
+] 
+
+#Dodato
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
